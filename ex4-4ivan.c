b6dadef417965e35b89544cab54bb35884c9cba5
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-09 17:24:02 -0500

    Create ex4-4ivan.c

---
+++ ex4-4ivan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+printf("H       H  E E E E E  L          L           O O O O O\n");
+printf("H       H  E          L          L           O       O\n");
+printf("H       H  E          L          L           O       O\n");
+printf("H H H H H  E E E E E  L          L           O       O\n");
+printf("H       H  E          L          L           O       O\n");
+printf("H       H  E          L          L           O       O\n");
+printf("H       H  E E E E E  L L L L L  L L L L L   O O O O O\n");
+
+
+return 0;
+}
