5b9008511f44b8302bd02d14ca83734f260c6aae
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-09 16:45:03 -0500

    Create ex4-1Ivan.c

---
+++ ex4-1Ivan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void){ 
+//in this case i just need to use printf
+    printf("Name: Ángel Mayo\n");
+    printf("social security number: 7034879\n");
+    printf("Date of birth: 07/11/2001\n");
+ return 0;
+
+  }
