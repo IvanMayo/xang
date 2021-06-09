db3980bf5917200ea6c76e0085392bbd330676a4
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-09 17:40:54 -0500

    Create ex4-5ivan.c

---
+++ ex4-5ivan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+
+int main(void){
+  //Print a floating-point number using the %d conversion
+float fp;
+fp = 7.79;
+
+printf("Float is %d\n", fp);
+
+  //Print an integer using the %f conversion
+int inte;
+inte = 4;
+
+printf("Integer is %f\n",inte);
+
+  //Print a character using the %d conversion
+
+char cha ='a';
+printf("Character is %d", cha);
+
+}
