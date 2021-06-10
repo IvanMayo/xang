3171f2a0901b43616e09938dd1070f0192930056
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-10 12:03:09 -0500

    Update sass.c

--- sass.c
+++ sass.c
@@ -26,3 +26,22 @@ int main() {
     printf("The name is %s\n", full);
     return (0);
 }
+
+
+#include <stdio.h>
+char  line[100];   /* input line from console */
+int   value;       /* a value to double */
+int   value2;
+float value3;
+
+int main()
+{
+    printf("Enter three values (last is float): ");
+
+    fgets(line, sizeof(line), stdin); //para que sirve esta linea?
+    sscanf(line, "%d %d %f", &value,&value2,&value3);
+
+    printf("Twice %d is %d\n", value, value * 2);
+    printf("another value2 is %d value3 is %f\n",value2,value3);
+    return (0);
+}
